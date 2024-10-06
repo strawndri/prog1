ad5f458548c6231044f55a20222d92f95d5e1e9d
Andrieli Gonçalves <andrieliluci@gmail.com>
2024-10-06 07:20:56 -0300

    [tp2] revisar e adicionar comentários no tp2.c

--- tp2/tp2.c
+++ tp2/tp2.c
@@ -4,13 +4,16 @@
  * Feito em 24/09/2024 para a disciplina CI1001 - Programação 1.
 */
 
-/* coloque aqui seus includes (primeiro os <...>, depois os "...") */
 #include <stdio.h>
 #include <stdlib.h>
 #include "racional.h"
 
 #define N 100
 
+/* Imprime os elementos de um vetor separando-os por um espaço em branco
+ * O último elemento é impresso separadamente para evitar que um
+ * espaço em branco extra seja printado. 
+*/
 void imprime_vetor(struct racional vetor[], int n)
 { 
   printf("VETOR = ");
@@ -22,38 +25,45 @@ void imprime_vetor(struct racional vetor[], int n)
   printf("\n");
 }
 
+/* Remoção de elementos x/0 presentes no vetor de racionais */
 void elimina_invalidos(struct racional vetor[], int *n)
 { 
   int i = 0;
+  /* Sempre que um elemento inválido é encontrado, ele é substituído
+   * pelo último elemento do vetor. Toda vez que isso ocorre o tamanho do
+   * vetor é diminuído em uma unidade.
+  */
   while (i < *n) {
     if (!valido_r(vetor[i])) {
       vetor[i] = vetor[*n - 1];
       (*n)--;
     } else {
-      i++;
+      i++; /* Só avançamos a posição se a atual contém um racional válido */
     }
   }
 }
 
+/* Algoritmo Bubble Sort recursivo */
 void ordena_vetor(struct racional vetor[], int n)
 {  
-
   if (n == 1)
     return;
 
-  int j = 0;
+  int trocas = 0;
   struct racional aux;
 
+  /* Compara elementos do vetor e troca-os se o atual é maior que o próximo */
   for (int i = 0; i < n - 1; i++) {
     if (compara_r(vetor[i], vetor[i + 1]) > 0) {
       aux = vetor[i];
       vetor[i] = vetor[i + 1];
       vetor[i + 1] = aux;
-      j++;
+      trocas++;
     }
   }
   
-  if (j == 0)
+  /* Se nenhuma troca ocorreu, o vetor já está ordenado */
+  if (!trocas)
     return;
 
   ordena_vetor(vetor, n - 1);
@@ -68,6 +78,9 @@ int main ()
 
   struct racional vetor[N];
 
+  /* Certifica que o usuário informou o número no intervalo esperado:
+   * 0 < n < 100 
+  */
   do {
     scanf("%d", &n);
   } while (n <= 0 || n >= 100);
@@ -97,5 +110,5 @@ int main ()
   imprime_r(soma);
   printf("\n");
 
-  return (0) ;
+  return 0;
 }
