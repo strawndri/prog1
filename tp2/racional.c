820cd350d8a50a6860461978444bdfd198be416b
Andrieli <andrieliluci@gmail.com>
2024-10-10 08:31:17 -0300

    [tp2] padronizar indentação e uso de chaves

--- tp2/racional.c
+++ tp2/racional.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * Tipos Abstratos de Dados - TADs
  * Arquivo de implementação para TAD racional.
  * Feito em 20/09/2024 para a disciplina CI1001 - Programação 1.
@@ -6,7 +6,7 @@
  * Este arquivo deve conter as implementações das funções cujos protótipos
  * foram definidos em racional.h. Neste arquivo também podem ser definidas
  * funções auxiliares para facilitar a implementação daquelas funções.
-*/
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,8 +14,8 @@
 
 /* Retorna um número aleatório entre min e max, inclusive
  * (max - min + 1) gera um número entre 0 e (max - min)
- * Somando min, temos o deslocamento do resultado para o intervalo [min, max] 
-*/
+ * Somando min, temos o deslocamento do resultado para o intervalo [min, max]
+ */
 long aleat(long min, long max)
 {
   return rand() % (max - min + 1) + min;
@@ -24,9 +24,8 @@ long aleat(long min, long max)
 /* Máximo Divisor Comum entre a e b pelo método de Euclides */
 long mdc(long a, long b)
 {
-  if (b == 0) {
+  if (b == 0)
     return a;
-  }
 
   return mdc(b, a % b);
 }
@@ -44,18 +43,18 @@ struct racional simplifica_r(struct racional r)
   long numerador, denominador;
 
   /* Verifica e retorna o racional se ele for inválido */
-  if (!valido_r(r)) {
+  if (!valido_r(r))
     return r;
-  } 
-  
+
   resultado_mdc = mdc(r.num, r.den);
 
   /* Simplifica o numerador e o denominador dividindo-os pelo MDC */
-  numerador = r.num / resultado_mdc; 
-  denominador = r.den / resultado_mdc; 
-  
+  numerador = r.num / resultado_mdc;
+  denominador = r.den / resultado_mdc;
+
   /* Ajusta o sinal para garantir que o denominador seja positivo */
-  if (denominador < 0) {
+  if (denominador < 0)
+  {
     numerador = -numerador;
     denominador = -denominador;
   }
@@ -79,7 +78,7 @@ int valido_r(struct racional r)
 
 /* Retorna um número racional aleatório na forma simplificada */
 struct racional sorteia_r(long min, long max)
-{ 
+{
   long numerador = aleat(min, max);
   long denominador = aleat(min, max);
 
@@ -91,35 +90,33 @@ void imprime_r(struct racional r)
 {
   struct racional simplificado = simplifica_r(r);
 
-  if (!valido_r(simplificado)) {
+  if (!valido_r(simplificado))
     printf("NaN");
-    /* O else-if a seguir resolve tais problemas: 
-    * 1. Numerador igual a 0
-    * 2. Denominador igual a 1
-    * 3. Numerador e denominador iguais 
-    * No caso do item 3, possivelmente ele cairá no item 2, já que anteriormente
-    * aconteceu a simplificação do número racional, levando à situação 1/1
-    */
-  } else if (!simplificado.num || simplificado.den == 1) {
-      printf("%ld", simplificado.num);
-  } else {
-      printf("%ld/%ld", simplificado.num, simplificado.den);
-  }
+  /* O else-if a seguir resolve tais problemas:
+   * 1. Numerador igual a 0
+   * 2. Denominador igual a 1
+   * 3. Numerador e denominador iguais
+   * No caso do item 3, possivelmente ele cairá no item 2, já que anteriormente
+   * aconteceu a simplificação do número racional, levando à situação 1/1
+   */
+  else if (!simplificado.num || simplificado.den == 1)
+    printf("%ld", simplificado.num);
+  else
+    printf("%ld/%ld", simplificado.num, simplificado.den);
 }
 
 /* Compara dois racionais r1 e r2:
  *  2: r1 ou r2 forem inválidos
  * -1: r1 < r2
  *  0: r1 = r2
- *  1: r1 > r2 
-*/
+ *  1: r1 > r2
+ */
 int compara_r(struct racional r1, struct racional r2)
-{ 
+{
   int prod_cruzado_1, prod_cruzado_2;
 
-  if (!valido_r(r1) || !valido_r(r2)) {
+  if (!valido_r(r1) || !valido_r(r2))
     return -2;
-  } 
 
   r1 = simplifica_r(r1);
   r2 = simplifica_r(r2);
@@ -128,27 +125,25 @@ int compara_r(struct racional r1, struct racional r2)
   prod_cruzado_1 = r1.num * r2.den;
   prod_cruzado_2 = r2.num * r1.den;
 
-  if (prod_cruzado_1 > prod_cruzado_2) {
+  if (prod_cruzado_1 > prod_cruzado_2)
     return 1;
-  } else if (prod_cruzado_1 < prod_cruzado_2) {
+  else if (prod_cruzado_1 < prod_cruzado_2)
     return -1;
-  } else {
+  else
     return 0;
-  }
 }
 
 /* Retorna a soma dos racionais r1 e r2 no parâmetro *r3 */
 int soma_r(struct racional r1, struct racional r2, struct racional *r3)
-{ 
+{
   long resultado_mmc, numerador;
 
   /* Verifica se r1 ou r2 são inválidos ou r3 é um ponteiro nulo */
-  if (!valido_r(r1) || !valido_r(r2) || r3 == NULL) {
+  if (!valido_r(r1) || !valido_r(r2) || r3 == NULL)
     return 0;
-  }
 
   resultado_mmc = mmc(r1.den, r2.den);
-  numerador = ((resultado_mmc * r1.num / r1.den) + 
+  numerador = ((resultado_mmc * r1.num / r1.den) +
                (resultado_mmc * r2.num / r2.den));
 
   r3->num = numerador;
@@ -163,12 +158,11 @@ int subtrai_r(struct racional r1, struct racional r2, struct racional *r3)
   long resultado_mmc, numerador;
 
   /* Verifica se r1 ou r2 são inválidos ou r3 é um ponteiro nulo */
-  if (!valido_r(r1) || !valido_r(r2) || r3 == NULL) {
+  if (!valido_r(r1) || !valido_r(r2) || r3 == NULL)
     return 0;
-  }
 
   resultado_mmc = mmc(r1.den, r2.den);
-  numerador = ((resultado_mmc * r1.num / r1.den) - 
+  numerador = ((resultado_mmc * r1.num / r1.den) -
                (resultado_mmc * r2.num / r2.den));
 
   r3->num = numerador;
@@ -179,11 +173,10 @@ int subtrai_r(struct racional r1, struct racional r2, struct racional *r3)
 
 /* Retorna a multiplicacao dos racionais r1 e r2 no parametro *r3 */
 int multiplica_r(struct racional r1, struct racional r2, struct racional *r3)
-{ 
+{
   /* Verifica se r1 ou r2 são inválidos ou r3 é um ponteiro nulo */
-  if (!valido_r(r1) || !valido_r(r2) || r3 == NULL) {
+  if (!valido_r(r1) || !valido_r(r2) || r3 == NULL)
     return 0;
-  }
 
   r3->num = r1.num * r2.num;
   r3->den = r1.den * r2.den;
@@ -193,14 +186,13 @@ int multiplica_r(struct racional r1, struct racional r2, struct racional *r3)
 
 /* Retorna a divisao dos racionais r1 e r2 no parametro *r3 */
 int divide_r(struct racional r1, struct racional r2, struct racional *r3)
-{ 
-  /* Verifica se r1 ou r2 são inválidos 
-   * ou o numerador de r2 é zero 
-   * ou r3 é um ponteiro nulo 
-  */
-  if (!valido_r(r1) || !valido_r(r2) || !r2.num || r3 == NULL) {
+{
+  /* Verifica se r1 ou r2 são inválidos
+   * ou o numerador de r2 é zero
+   * ou r3 é um ponteiro nulo
+   */
+  if (!valido_r(r1) || !valido_r(r2) || !r2.num || r3 == NULL)
     return 0;
-  }
 
   r3->num = r1.num * r2.den;
   r3->den = r1.den * r2.num;
--- tp2/tp2.c
+++ tp2/tp2.c
@@ -1,8 +1,8 @@
-/* 
+/*
  * Tipos Abstratos de Dados - TADs
  * Arquivo do programa principal, que usa o TAD racional.
  * Feito em 24/09/2024 para a disciplina CI1001 - Programação 1.
-*/
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,34 +12,39 @@
 
 /* Imprime os elementos de um vetor separando-os por um espaço em branco
  * O último elemento é impresso separadamente para evitar que um
- * espaço em branco extra seja printado 
-*/
+ * espaço em branco extra seja printado
+ */
 void imprime_vetor(struct racional vetor[], int n)
-{ 
+{
   if (!n)
     return;
 
-  for (int i = 0; i < n - 1; i++) {
+  for (int i = 0; i < n - 1; i++)
+  {
     imprime_r(vetor[i]);
     printf(" ");
   }
 
-  imprime_r(vetor[n-1]);
+  imprime_r(vetor[n - 1]);
 }
 
 /* Remoção de elementos x/0 (inválidos) presentes no vetor de racionais */
 void elimina_invalidos(struct racional vetor[], int *n)
-{ 
+{
   int i = 0;
   /* Sempre que um elemento inválido é encontrado, ele é substituído
    * pelo último elemento do vetor. Toda vez que isso ocorre o tamanho do
    * vetor é diminuído em uma unidade.
-  */
-  while (i < *n) {
-    if (!valido_r(vetor[i])) {
+   */
+  while (i < *n)
+  {
+    if (!valido_r(vetor[i]))
+    {
       vetor[i] = vetor[*n - 1];
       (*n)--;
-    } else {
+    }
+    else
+    {
       i++; /* Só avançamos a posição se a atual contém um racional válido */
     }
   }
@@ -47,7 +52,7 @@ void elimina_invalidos(struct racional vetor[], int *n)
 
 /* Algoritmo Bubble Sort recursivo */
 void ordena_vetor(struct racional vetor[], int n)
-{  
+{
   int trocas;
   struct racional aux;
 
@@ -57,15 +62,17 @@ void ordena_vetor(struct racional vetor[], int n)
   trocas = 0;
 
   /* Compara elementos do vetor e troca-os se o atual é maior que o próximo */
-  for (int i = 0; i < n - 1; i++) {
-    if (compara_r(vetor[i], vetor[i + 1]) > 0) {
+  for (int i = 0; i < n - 1; i++)
+  {
+    if (compara_r(vetor[i], vetor[i + 1]) > 0)
+    {
       aux = vetor[i];
       vetor[i] = vetor[i + 1];
       vetor[i + 1] = aux;
       trocas++;
     }
   }
-  
+
   /* Se nenhuma troca ocorreu, o vetor já está ordenado */
   if (!trocas)
     return;
@@ -74,21 +81,23 @@ void ordena_vetor(struct racional vetor[], int n)
 }
 
 /* programa principal */
-int main ()
-{ 
+int main()
+{
   int n;
   long numerador, denominador;
   struct racional racional, soma;
   struct racional vetor[N];
 
   /* Certifica que o usuário informou o número no intervalo esperado:
-   * 0 < n < 100 
-  */
-  do {
+   * 0 < n < 100
+   */
+  do
+  {
     scanf("%d", &n);
   } while (n <= 0 || n >= 100);
 
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < n; i++)
+  {
     scanf("%ld %ld", &numerador, &denominador);
     racional = cria_r(numerador, denominador);
     vetor[i] = racional;
@@ -99,22 +108,20 @@ int main ()
   printf("\n");
 
   elimina_invalidos(vetor, &n);
- 
+
   printf("VETOR = ");
   imprime_vetor(vetor, n);
   printf("\n");
 
   ordena_vetor(vetor, n);
-  
+
   printf("VETOR = ");
   imprime_vetor(vetor, n);
   printf("\n");
 
   soma = cria_r(0, 1);
-
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < n; i++)
     soma_r(soma, vetor[i], &soma);
-  }
 
   printf("SOMA = ");
   imprime_r(soma);
