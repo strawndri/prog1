f0b41b9476da5971eee1a8bd9050cc691c1840d1
Andrieli Gonçalves <andrieliluci@gmail.com>
2024-10-12 16:39:26 -0300

    [tp3] criar funções simplifica_r e cria_r

--- tp3/racional.c
+++ tp3/racional.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * Tipos Abstratos de Dados - TADs
  * Arquivo de implementação para TAD racional.
  * Feito em 20/09/2024 para a disciplina CI1001 - Programação 1.
@@ -6,16 +6,18 @@
  * Este arquivo deve conter as implementações das funções cujos protótipos
  * foram definidos em racional.h. Neste arquivo também podem ser definidas
  * funções auxiliares para facilitar a implementação daquelas funções.
-*/
+ */
 
 /* coloque aqui seus includes (primeiro os <...>, depois os "...") */
 #include <stdio.h>
+#include <stdlib.h>
+#include "racional.h"
 
 /*
  * Implemente aqui as funcoes definidas no racionais.h; caso precise,
  * pode definir aqui funcoes auxiliares adicionais, que devem ser usadas
  * somente neste arquivo.
-*/
+ */
 
 /* Máximo Divisor Comum entre a e b pelo método de Euclides */
 long mdc(long a, long b)
@@ -37,9 +39,45 @@ long mmc(long a, long b)
  * Retorna 1 em sucesso e 0 se r for inválido ou o ponteiro for nulo.
  * Se ambos numerador e denominador forem negativos, o resultado é positivo.
  * Se o denominador for negativo, o sinal deve migrar para o numerador. */
-int simplifica_r (struct racional *r)
+int simplifica_r(struct racional *r)
 {
-  /* implemente aqui */
+  long resultado_mdc;
+  long numerador, denominador;
+
+  /* Verifica e retorna o racional se ele for inválido */
+  if (r == NULL || !invalido_r(*r))
+    return 0;
+
+  resultado_mdc = mdc(r->num, r->den);
+
+  /* Simplifica o numerador e o denominador dividindo-os pelo MDC */
+  numerador = r->num / resultado_mdc;
+  denominador = r->den / resultado_mdc;
+
+  /* Ajusta o sinal para garantir que o denominador seja positivo */
+  if (denominador < 0)
+  {
+    numerador = -numerador;
+    denominador = -denominador;
+  }
+
+  r->num = numerador;
+  r->den = denominador;
+
+  return 1;
 }
 
-/* implemente as demais funções de racional.h aqui */
+/* Cria um número racional com o numerador e denominador indicados
+ * e retorna um ponteiro que aponta para ele.
+ * A memória para o número racional deve ser alocada dinamicamente
+ * por esta função. Retorna NULL se não conseguiu alocar a memória. */
+struct racional *cria_r(long numerador, long denominador)
+{
+  struct racional *novo = malloc(sizeof(struct racional));
+
+  if (novo == NULL)
+    return NULL;
+
+  novo->num = numerador;
+  novo->den = denominador;
+}
