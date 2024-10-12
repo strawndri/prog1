2b6d587a887715e27502249104f637a50168ff03
Andrieli Gonçalves <andrieliluci@gmail.com>
2024-10-12 18:06:17 -0300

    [tp3] criar restante das funções de racional.c e tp3.c

--- tp3/racional.c
+++ tp3/racional.c
@@ -45,7 +45,7 @@ int simplifica_r(struct racional *r)
   long numerador, denominador;
 
   /* Verifica e retorna o racional se ele for inválido */
-  if (r == NULL || !invalido_r(*r))
+  if (r == NULL || !valido_r(r))
     return 0;
 
   resultado_mdc = mdc(r->num, r->den);
@@ -73,11 +73,156 @@ int simplifica_r(struct racional *r)
  * por esta função. Retorna NULL se não conseguiu alocar a memória. */
 struct racional *cria_r(long numerador, long denominador)
 {
-  struct racional *novo = malloc(sizeof(struct racional));
+  struct racional *novo = (struct racional *)malloc(sizeof(struct racional));
 
   if (novo == NULL)
     return NULL;
 
   novo->num = numerador;
   novo->den = denominador;
+
+  return novo;
+}
+
+/* Libera a memória alocada para o racional apontado por r */
+void destroi_r(struct racional *r)
+{
+  if (r != NULL)
+    free(r);
+}
+
+/* Retorna 1 se o racional r for válido ou 0 se for inválido. Um racional
+ * é inválido se o denominador for zero ou se ele não tiver sido alocado. */
+int valido_r(struct racional *r)
+{
+  return (r->den != 0 && r != NULL);
+}
+
+/* Imprime um racional r, respeitando estas regras:
+   - o racional deve estar na forma simplificada;
+   - não use espacos em branco e não mude de linha;
+   - o formato de saída deve ser "num/den", a menos dos casos abaixo;
+     - se o ponteiro for nulo, imprime a mensagem "NULL";
+     - se o racional for inválido, imprime a mensagem "NaN" (Not a Number);
+     - se o numerador for 0, imprime somente "0";
+     - se o denominador for 1, imprime somente o numerador;
+     - se o numerador e denominador forem iguais, imprime somente "1";
+     - se o racional for negativo, o sinal é impresso antes do número;
+     - se numerador e denominador forem negativos, o racional é positivo. */
+void imprime_r(struct racional *r)
+{
+  if (r == NULL)
+  {
+    printf("NULL");
+    return;
+  }
+
+  simplifica_r(r);
+
+  if (!valido_r(r))
+    printf("NaN");
+  /* O else-if a seguir resolve tais problemas:
+   * 1. Numerador igual a 0
+   * 2. Denominador igual a 1
+   * 3. Numerador e denominador iguais
+   * No caso do item 3, possivelmente ele cairá no item 2, já que anteriormente
+   * aconteceu a simplificação do número racional, levando à situação 1/1
+   */
+  else if (!r->num || r->den == 1)
+    printf("%ld", r->num);
+  else
+    printf("%ld/%ld", r->num, r->den);
+}
+
+/* Compara dois números racionais r1 e r2.
+ * Retorna -2 se r1 ou r2 for inválido ou se o respectivo ponteiro for nulo.
+ * Retorna -1 se r1 < r2; 0 se r1 = r2; 1 se r1 > r2.
+ * Atenção: faça a comparação normalizando os denominadores pelo MMC.
+ * Fazer a comparação baseado na divisão do numerador pelo denominador
+ * pode gerar erro de arredondamento e falsear o resultado. */
+int compara_r(struct racional *r1, struct racional *r2)
+{
+  long resultado_mmc, n1, n2;
+
+  if (!valido_r(r1) || !valido_r(r2) || r1 == NULL || r2 == NULL)
+    return -2;
+
+  resultado_mmc = mmc(r1->den, r2->den);
+
+  n1 = resultado_mmc * r1->num / r1->den;
+  n2 = resultado_mmc * r2->num / r2->den;
+
+  if (n1 < n2)
+    return -1;
+  else if (n1 > n2)
+    return 1;
+  else
+    return 0;
+}
+
+/* Retorna a soma dos racionais r1 e r2 no parâmetro *r3 */
+int soma_r(struct racional *r1, struct racional *r2, struct racional *r3)
+{
+  long resultado_mmc, numerador;
+
+  /* Verifica se r1 ou r2 são inválidos ou r3 é um ponteiro nulo */
+  if (!valido_r(r1) || !valido_r(r2) || r3 == NULL)
+    return 0;
+
+  resultado_mmc = mmc(r1->den, r2->den);
+  numerador = ((resultado_mmc * r1->num / r1->den) +
+               (resultado_mmc * r2->num / r2->den));
+
+  r3->num = numerador;
+  r3->den = resultado_mmc;
+
+  return 1;
+}
+
+/* Retorna a subtracao dos racionais r1 e r2 no parametro *r3 */
+int subtrai_r(struct racional *r1, struct racional *r2, struct racional *r3)
+{
+  long resultado_mmc, numerador;
+
+  /* Verifica se r1 ou r2 são inválidos ou r3 é um ponteiro nulo */
+  if (!valido_r(r1) || !valido_r(r2) || r3 == NULL)
+    return 0;
+
+  resultado_mmc = mmc(r1->den, r2->den);
+  numerador = ((resultado_mmc * r1->num / r1->den) -
+               (resultado_mmc * r2->num / r2->den));
+
+  r3->num = numerador;
+  r3->den = resultado_mmc;
+
+  return 1;
+}
+
+/* Retorna a multiplicacao dos racionais r1 e r2 no parametro *r3 */
+int multiplica_r(struct racional *r1, struct racional *r2, struct racional *r3)
+{
+  /* Verifica se r1 ou r2 são inválidos ou r3 é um ponteiro nulo */
+  if (!valido_r(r1) || !valido_r(r2) || r3 == NULL)
+    return 0;
+
+  r3->num = r1->num * r2->num;
+  r3->den = r1->den * r2->den;
+
+  return 1;
+}
+
+/* Retorna a divisao dos racionais r1 e r2 no parametro *r3 */
+int divide_r(struct racional *r1, struct racional *r2, struct racional *r3)
+{
+  /* Verifica se r1 ou r2 são inválidos
+   * ou o numerador de r2 é zero
+   * ou r3 é um ponteiro nulo
+   */
+  if (!valido_r(r1) || !valido_r(r2) || !r2->num || r3 == NULL)
+    return 0;
+
+  r3->num = r1->num * r2->den;
+  r3->den = r1->den * r2->num;
+
+  return 1;
 }
--- tp3/tp3.c
+++ tp3/tp3.c
@@ -1,19 +1,149 @@
-/* 
+/*
  * Tipos Abstratos de Dados - TADs
  * Arquivo do programa principal, que usa o TAD racional.
  * Feito em 24/09/2024 para a disciplina CI1001 - Programação 1.
-*/
+ */
 
 /* coloque aqui seus includes (primeiro os <...>, depois os "...") */
 #include <stdio.h>
+#include <stdlib.h>
+#include "racional.h"
 
-/* coloque aqui as funções auxiliares que precisar neste arquivo */
-
-/* programa principal */
-int main ()
+/* Imprime os elementos de um vetor separando-os por um espaço em branco
+ * O último elemento é impresso separadamente para evitar que um
+ * espaço em branco extra seja printado
+ */
+void imprime_vetor(struct racional **vetor, int n)
 {
-  /* a completar! */
+  if (!n)
+    return;
 
-  return (0) ;
+  for (int i = 0; i < n - 1; i++)
+  {
+    imprime_r(vetor[i]);
+    printf(" ");
+  }
+
+  imprime_r(vetor[n - 1]);
 }
 
+/* Remoção de elementos x/0 (inválidos) presentes no vetor de racionais */
+void elimina_invalidos(struct racional **vetor, int *n)
+{
+  int i = 0;
+  /* Sempre que um elemento inválido é encontrado, ele é substituído
+   * pelo último elemento do vetor. Toda vez que isso ocorre o tamanho do
+   * vetor é diminuído em uma unidade.
+   */
+  while (i < *n)
+  {
+    if (!valido_r(vetor[i]))
+    {
+      vetor[i] = vetor[*n - 1];
+      (*n)--;
+    }
+    else
+    {
+      i++; /* Só avançamos a posição se a atual contém um racional válido */
+    }
+  }
+}
+
+/* Algoritmo Bubble Sort recursivo */
+void ordena_vetor(struct racional **vetor, int n)
+{
+  int trocas;
+  struct racional *aux;
+
+  if (n == 1)
+    return;
+
+  trocas = 0;
+
+  /* Compara elementos do vetor e troca-os se o atual é maior que o próximo */
+  for (int i = 0; i < n - 1; i++)
+  {
+    if (compara_r(vetor[i], vetor[i + 1]) == 1)
+    {
+      aux = vetor[i];
+      vetor[i] = vetor[i + 1];
+      vetor[i + 1] = aux;
+      trocas++;
+    }
+  }
+
+  /* Se nenhuma troca ocorreu, o vetor já está ordenado */
+  if (!trocas)
+    return;
+
+  ordena_vetor(vetor, n - 1);
+}
+
+/* programa principal */
+int main()
+{
+  int n;
+  long numerador, denominador;
+  struct racional *soma;
+  struct racional **vetor;
+
+  /* Certifica que o usuário informou o número no intervalo esperado:
+   * 0 < n < 100
+   */
+  do
+  {
+    scanf("%d", &n);
+  } while (n <= 0 || n >= 100);
+
+  vetor = (struct racional **) malloc(n * sizeof(struct racional *));
+  if (vetor == NULL)
+    return 0;
+
+  for (int i = 0; i < n; i++)
+  {
+    scanf("%ld %ld", &numerador, &denominador);
+    vetor[i] = cria_r(numerador, denominador);
+    
+    if (vetor[i] == NULL)
+      return 0;
+  }
+
+  printf("VETOR = ");
+  imprime_vetor(vetor, n);
+  printf("\n");
+
+  elimina_invalidos(vetor, &n);
+
+  printf("VETOR = ");
+  imprime_vetor(vetor, n);
+  printf("\n");
+
+  ordena_vetor(vetor, n);
+
+  printf("VETOR = ");
+  imprime_vetor(vetor, n);
+  printf("\n");
+
+  soma = cria_r(0, 1);
+  for (int i = 0; i < n; i++)
+    soma_r(soma, vetor[i], soma);
+
+  printf("SOMA = ");
+  imprime_r(soma);
+  printf("\n");
+
+  for (int i = 0; i < n; i++)
+  {
+    if (vetor[i] != NULL)
+      destroi_r(vetor[i]);
+  }
+
+  printf("VETOR = ");
+  imprime_vetor(vetor, n);
+  printf("\n");
+
+  free(vetor);
+  destroi_r(soma);
+
+  return 0;
+}
