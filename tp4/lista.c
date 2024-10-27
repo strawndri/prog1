b76a14792ff02edd154c77e278522150de14e078
Andrieli Gonçalves <andrieliluci@gmail.com>
2024-10-27 08:14:24 -0300

    [tp4] criar funções cria, destroi e insere

--- tp4/lista.c
+++ tp4/lista.c
@@ -5,4 +5,73 @@
 //
 // Implementação com lista encadeada dupla não-circular
 
+#include <stdio.h>
+#include <stdlib.h>
+#include "lista.h"
 
+// Cria uma lista vazia.
+// Retorno: ponteiro p/ a lista ou NULL em erro.
+struct lista_t *lista_cria()
+{
+  struct lista_t *lst;
+
+  // Aloca struct referente à lista
+  lst = malloc(sizeof(struct lista_t));
+  if (!lst)
+    return NULL;
+
+  lst->prim = NULL;
+  lst->ult = NULL;
+  lst->tamanho = 0;
+
+  return lst;
+}
+
+// Remove todos os itens da lista e libera a memória.
+// Retorno: NULL.
+struct lista_t *lista_destroi(struct lista_t *lst)
+{
+  int elemento;
+  
+  if (!lst)
+    return NULL;
+
+  // criar lista_tamanho e lista_retira
+  while (lista_tamanho(lst) > 0)
+    lista_retira(lst, &elemento);
+
+  free(lst);
+  
+  return NULL;
+}
+
+// Insere o item na lista na posição indicada;
+// se a posição for além do fim da lista ou for -1, insere no fim.
+// Retorno: número de itens na lista após a operação ou -1 em erro.
+int lista_insere (struct lista_t *lst, int item, int pos)
+{
+  struct item_t *aux;
+
+  if (!lst)
+    return -1;
+
+  // Cria e inicia novo item
+  aux = malloc(sizeof(struct item_t));
+  if (!aux)
+    return -1;
+  aux->valor = item;
+  aux->prox = lst->prim;
+  aux->ant = NULL; // O anterior deve ser nulo porque é o primeiro nó
+  
+
+  if (!lst->prim)
+  {
+    lst->prim = aux;
+    lst->ult = aux;
+  }
+  else
+  {
+    lst->prim->ant = aux;
+    lst->prim = aux;
+  }
+}
