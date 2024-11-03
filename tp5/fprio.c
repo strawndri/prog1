430e800dcf4dbd41a7c6697d784155260b66fe49
Andrieli Gonçalves <andrieliluci@gmail.com>
2024-11-03 16:04:53 -0300

    [tp5] criar função fprio_cria

--- tp5/fprio.c
+++ tp5/fprio.c
@@ -2,4 +2,23 @@
 // Carlos Maziero, DINF/UFPR, Out 2024
 // Implementação com lista encadeada simples
 
-// A COMPLETAR
+#include <stdio.h>
+#include <stdlib.h>
+#include "fprio.h"
+
+// Cria uma fila vazia.
+// Retorno: ponteiro para a fila criada ou NULL se erro.
+struct fprio_t *fprio_cria()
+{
+  struct fprio_t *fprio;
+
+  // Aloca struct referente à fila
+  fprio = malloc(sizeof(struct fprio_t));
+  if (!fprio)
+    return NULL;
+
+  fprio->prim = NULL;
+  fprio->num = 0;
+
+  return fprio;
+}
